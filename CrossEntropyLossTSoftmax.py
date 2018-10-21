05b7f4ccf2b56d6d8ef7807a5d1d38c9c02fd19a
Jose Mario Carranza <maeotaku@gmail.com>
2018-10-21 13:16:08 -0600

    fixning bug

--- CrossEntropyLossTSoftmax.py
+++ CrossEntropyLossTSoftmax.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-from TSoftmax import TSoftmax
+from .TSoftmax import TSoftmax
 
 class CrossEntropyLossTSoftmax(TSoftmax):
 
